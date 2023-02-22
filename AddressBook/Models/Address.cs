d7116493b8ac167e69706a328aa69fe3be9ecd8e
Shovon Raul <shovon.r@keka.com>
2023-02-22 14:53:40 +0530

    Add project files.

---
+++ AddressBook/Models/Address.cs
@@ -0,0 +1,22 @@
+﻿namespace AddressBook.Models
+{
+    public class Address
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int MobileNumber { get; set; }
+        public int LandLineNumber { get; set; }
+        public string Website { get; set; }
+        public string AddressDetails { get; set; }
+
+        public Address(string Name,string Email,int MobileNumber, int LandLineNumber, string Website, string AddressDetails) 
+        {
+            this.Name = Name;
+            this.Email = Email;
+            this.MobileNumber = MobileNumber;
+            this.LandLineNumber = LandLineNumber;
+            this.Website = Website;
+            this.AddressDetails = AddressDetails;
+        }
+    }
+}
